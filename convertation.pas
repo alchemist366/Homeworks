ea5b50b684c67367f7dd4cb3669e7ad12f16f280
alchemist366 <salinsasha366@gmail.com>
2015-12-06 18:30:26 +0300

    06.12.2015

---
+++ convertation.pas
@@ -0,0 +1,26 @@
+var
+c:char;
+x: integer;
+function is_digit(c: char): boolean;
+begin
+  is_digit := (c >= '0') and (c <= '9');
+end;
+
+function convertation(c: char): integer;
+begin
+
+  if (is_digit(c)) then 
+  begin 
+    convertation:= integer(c) - integer('0'); 
+  end
+  
+  else 
+    begin
+      convertation:= -1;
+    end;
+end;
+begin
+readln(c);
+x:= convertation(c);
+writeln(x);
+end.
