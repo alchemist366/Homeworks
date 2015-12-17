d3ea9c20502fb806c3bc4c41106bf3155192bb4f
alchemist366 <salinsasha366@gmail.com>
2015-12-17 17:34:37 +0300

    -

--- A1.pas
+++ A1.pas
@@ -1,47 +1,69 @@
-var 
-s1, s2:String;
-i, sum, k, l, j:integer;
-function razr(i:integer):integer;
 var
-j, f:integer;
+  n1, n2: String;
+  sum: integer;
+
+function step(i: integer): integer;
+var
+  j, f: integer;
 begin
-f:= 1;
- for j:= 1 to i do
- f:= f * 10; 
-razr:= f;
-end;
-begin
-readln(s1);
-readln(s2);
-l:= 0;
-if (Length(s1) >= Length(s2)) then
-begin
-j:= length(s2);
-for i:= Length(s1) downto 1 do
- begin
-  if (j > 0) then begin k:= ord(s2[j]) - 48; dec(j); end
-  else k:= 0;
-  if (k + ord(s1[i]) - 48 > 9) then begin 
-  sum:= (sum + (k + ord(s1[i]) - 48 + l) * razr(length(s1) - i)) mod razr(length(s1) - i + 1);
-  l:= 1;
-  end
-  else begin sum:= sum + (k + ord(s1[i]) - 48 + l) * razr(length(s1) - i); l:= 0; end;
- end;
-end
-else
-begin
-j:= length(s1);
-for i:= Length(s2) downto 1 do
- begin
-  if (j > 0) then begin k:= ord(s1[j]) - 48; dec(j); end
-  else k:= 0;
-  if (k + ord(s2[i]) - 48 > 9) then begin 
-  sum:= (sum + (k + ord(s2[i]) - 48 + l) * razr(length(s2) - i)) mod razr(length(s2) - i + 1);
-  l:= 1;
-  end
-  else begin sum:= sum + (k + ord(s2[i]) - 48 + l) * razr(length(s2) - i); l:= 0; end;
- end;
+  f := 1;
+  for j := 1 to i do
+    f := f * 10; 
+  step := f;
 end;
 
-writeln(sum);
+function INum(str: string; i: integer): integer;
+begin
+  INum := ord(str[i]) - 48;
+end;
+
+procedure sumNum(s1, s2: string; var sum: integer);
+var
+  i, k, l, j, x, y: integer;
+begin
+  
+  l := 0;
+  j := length(s2);
+  
+  for i := Length(s1) downto 1 do
+  begin
+    if (j > 0) then 
+    begin
+      y := INum(s2, j);
+      dec(j);
+    end
+    else 
+    begin
+      y := 0;
+    end;
+    
+    x := INum(s1, i);
+    if (y + x > 9) then begin
+      sum := (sum + (y + x + l) * step(length(s1) - i)) mod step(length(s1) - i + 1);
+      l := 1;
+    end
+    else 
+    begin
+      sum := sum + (y + x + l) * step(length(s1) - i); l := 0; 
+    end;
+  end;
+  
+end;
+
+begin
+  writeln('input first number');
+  readln(n1);
+  writeln('input second number');
+  readln(n2);
+  
+  if (Length(n1) >= Length(n2)) then
+  begin
+    sumNum(n1, n2, sum);
+  end
+  else
+  begin
+    sumNum(n2, n1, sum);
+  end;
+  
+  writeln(sum);
 end.
