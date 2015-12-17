0b88744d0f801d010d22b4d4270918e91e95989b
alchemist366 <salinsasha366@gmail.com>
2015-12-17 18:24:12 +0300

    -

--- A2.pas
+++ A2.pas
@@ -1,39 +1,90 @@
+﻿
+var
+  a: array  of integer;
+  prefix: array  of integer;
+  k, n, i: integer;
+  pereodic: boolean;
+  
+function CheckPeriodic(c: array of integer; n, k: integer): boolean;
+var
+f, j, i:integer;
+begin
+  f := 0;
+  j := 0;
+  for i := k  to n do
+  begin
+    inc(j);
+    if (c[i] = j) then
+    begin
+      if (j = k - 1) then 
+      begin
+        j := 0;
+      end;
+      inc(f);
+    end
+    else
+    begin
+      break;
+    end;
+  end;
+  
+  if (n - k + 1 = f) then
+  begin
+    CheckPeriodic := true;
+  end
+  else
+  begin
+    CheckPeriodic := false;
+  end;
+end;
 
-var 
-a: array  of integer;
-prefix: array  of integer;
-i, j, k, n, m, f: integer;
+procedure FindPrefix(var c: array of integer; a: array of integer; n:integer; var k: integer);
+var
+i, j: integer;
 begin
-writeln('Ââåñòè äëèíó ìàññèâà');
-readln(n);
-setlength(a, n + 1);
-setlength(prefix, n + 1);
-writeln('Ââåñòè ñàì ìàññèâ= ');
-for i:= 1 to n do
- begin
-  read(a[i]);
- end;
- k:= 1;
- j:= 1;
- prefix[1]:= 0;
-for i:= 2 to n do
-begin 
- if (a[i] = a[j]) then begin prefix[i]:= j; inc(j); end
- else begin j:= 1; prefix[i]:= 0; end;
- if (j > k) then k:= j;
+  k := 1;
+  j := 1;
+  c[1] := 0;
+  
+  for i := 2 to n do
+  begin
+    if (a[i] = a[j]) then 
+    begin
+      c[i] := j; 
+      inc(j);
+    end
+    else 
+    begin
+      j := 1; 
+      c[i] := 0;
+    end;
+    if (j > k) then 
+    begin
+      k := j;
+    end;
+  end;
 end;
-f:= 0;
-i:= k ;
-j:= 0;
-for i:= k  to n do
+
 begin
- inc(j);
- if (prefix[i] = j) then
- begin
-  if (j = k - 1) then j:=0; inc(f);
- end
- else break;
-end;
-if (n - k + 1 = f) then writeln('ïîñëåäîâàòåëüíîñòü ïåðèîäè÷íà ')
-else writeln('ïîñëåäîâàòåëüíîñòü íå ïåðèîäè÷íà ');
+  writeln('Write length of massive');
+  readln(n);
+  setlength(a, n + 1);
+  setlength(prefix, n + 1);
+  writeln('Write elements of massive');
+  for i := 1 to n do
+  begin
+    read(a[i]);
+  end;
+  
+  FindPrefix(prefix, a, n, k);
+  pereodic:= CheckPeriodic(prefix, n, k);
+  
+  if (pereodic) then
+  begin
+    writeln('array is periodic ');
+  end
+  else
+  begin
+    writeln('array is not periodic ');
+  end;
 end.
